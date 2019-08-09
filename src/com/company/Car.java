12607352070ad925b56aa6efade174b4def66848
AzamatAsanbekov <azamat.asanbekov18@gmail.com>
2019-08-09 16:06:25 +0600

    Initial commit

---
+++ src/com/company/Car.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public  class Car implements Comparable<Car> {
+    private String model;
+    private int year;
+    private int volume;
+
+    public Car(String model, int year, int volume) {
+        this.setModel(model);
+        this.setYear(year);
+        this.setVolume(volume);
+    }
+    @Override
+    public int compareTo(Car o) {
+        return this.getYear() - o.getYear();
+    }
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+}
